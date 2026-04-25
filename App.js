c7bfdeb47b65e8cf37a05cc1ac0f93758a3c3a13
Sebastian flores gonzalez <sebastianflg@Sebastians-MacBook-Air.local>
2026-04-25 11:00:46 -0600

    Subiendo archivos

--- App.js
+++ App.js
@@ -1,20 +1,118 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef, useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+import { CameraView, useCameraPermissions } from 'expo-camera';
 
 export default function App() {
+  const [facing, setFacing] = useState('back');
+  const [permission, requestPermission] = useCameraPermissions();
+  const [photo, setPhoto] = useState(null);
+  const cameraRef = useRef(null);
+
+  if (!permission) {
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>
+          Necesitamos tu permiso para acceder a la cámara
+        </Text>
+        <Button onPress={requestPermission} title="Conceder permiso" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing((current) => (current === 'back' ? 'front' : 'back'));
+  }
+
+  async function takePhoto() {
+    if (cameraRef.current) {
+      const picture = await cameraRef.current.takePictureAsync();
+      setPhoto(picture.uri);
+      console.log(picture.uri);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CameraView ref={cameraRef} style={styles.camera} facing={facing}>
+        <View style={styles.overlay}>
+          <TouchableOpacity style={styles.flipButton} onPress={toggleCameraFacing}>
+            <Text style={styles.text}>Voltear</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.captureButton} onPress={takePhoto}>
+            <View style={styles.innerCircle} />
+          </TouchableOpacity>
+        </View>
+      </CameraView>
+
+      {photo && <Image source={{ uri: photo }} style={styles.preview} />}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  overlay: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    paddingBottom: 40,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  controls: {
+    position: 'absolute',
+    bottom: 40,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
     alignItems: 'center',
+  },
+  flipButton: {
+    position: 'absolute',
+    top: 50,
+    right: 20,   
+    padding: 12,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    borderRadius: 10,
+  },
+  captureButton: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    borderWidth: 4,
+    borderColor: 'white',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(255,255,255,0.2)'
+  },
+  innerCircle: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: 'white',
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
+  },
+  preview: {
+    width: 120,
+    height: 160,
+    position: 'absolute',
+    top: 40,
+    right: 20,
+    borderRadius: 10,
   },
 });
